62e310c130252dd7297cdc464bef9805c94eec23
Jeeva Parkkavan <116168359+jeeva1429@users.noreply.github.com>
2025-08-18 17:20:03 +0530

    Update shell.c
    re-wrote the code for my understanding

--- shell.c
+++ shell.c
@@ -1,144 +1,86 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <fcntl.h>
 
-#define MAX_ARGS 12
-#define MAX_BUFF_SIZE 500
-
-enum CMD_TYPE {TRIVIAL, REDIRECT, PIPING, BACKGROUND};
-enum REDIR_TYPE {REDIR_IN, REDIR_OUT};
-
-struct cmd {
-    enum CMD_TYPE cmd_type;
-    char **argv;
-    struct redir_cmd *redir;
+struct cmd_exe_struct {
+    char *args[20];     // arguments (like ls, -l, etc.)
+    int is_redirected;  // 0 = no, 1 = yes
+    int redir_type;     // 1 = output (>), 0 = input (<)
+    char *file_path;    // file for redirection
 };
 
-
-struct redir_cmd {
-    enum REDIR_TYPE redir_type; // redirection type 
-    char* file_path; // file path to redirect 
+void execute_cmd(struct cmd_exe_struct *cmd) {
+    pid_t pid  = fork();
+    if (pid < 0) {
+        perror("Process creation failed!");
+        exit(0);
+    } else if (pid == 0) {
+        if(cmd->is_redirected) {
+        int file_fd;
+        if(cmd->redir_type == 1) {   // output ">"
+            file_fd = open(cmd->file_path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if(file_fd < 0) { perror("open failed"); exit(1); }
+            dup2(file_fd, STDOUT_FILENO);
+        } else {                     // input "<"
+            file_fd = open(cmd->file_path, O_RDONLY);
+            if(file_fd < 0) { perror("open failed"); exit(1); }
+            dup2(file_fd, STDIN_FILENO);
+        }
+        close(file_fd);  // close original FD after dup2
+        }
+        execvp(cmd->args[0], cmd->args);
+    }
+    else {
+        int status;
+        waitpid(pid, &status,0);
+    }
 };
 
-
-void handle_redirection(struct redir_cmd *rd) {
-    int newfd;
-    if (rd->redir_type == REDIR_OUT) {
-        newfd = open(rd->file_path, O_CREAT | O_TRUNC | O_WRONLY, 0644);
-        if (newfd < 0) {
-            perror(rd->file_path);
-            exit(1);
-        }
-        dup2(newfd, STDOUT_FILENO);
-    } else {
-        newfd = open(rd->file_path, O_RDONLY);
-        if (newfd < 0) {
-            perror(rd->file_path);
-            exit(1);
-        }
-        dup2(newfd, STDIN_FILENO);
-    }
-
-    close(newfd);
-}
-
-
-// i need to reuse the normal execution again in the redirection
-void exe_cmd(struct cmd *cmd) {
-    switch (cmd->cmd_type)
-    {
-    case TRIVIAL: 
-        pid_t pid;
-        pid = fork();
-        if(pid < 0) {
-            perror("Error: Process creation failed!");
-            exit(1);
-        }
-        else if(pid == 0) {
-            execvp(cmd->argv[0],cmd->argv);
-            perror("Command execution failed");
-            exit(1);
-
-        }
-        else {
-            wait(NULL);
-        }
-        break;
-    case REDIRECT:
-        pid_t pid1;
-        pid1 = fork();
-        if(pid1< 0) {
-            perror("Error: Process creation failed!");
-            exit(1);
-        }
-        else if(pid1 == 0) {
-            handle_redirection(cmd->redir);
-            execvp(cmd->argv[0],cmd->argv);
-            perror("Command execution failed");
-            exit(1);
-
-        }
-        else {
-            wait(NULL);
-        }
-    default:
-        break;
-    }
-
-    
-}
-
-
-struct cmd *parse_cmd(char *cmd_str, char **argv) {
-    struct cmd *cmd_struct = malloc(sizeof(struct cmd));
-
-    struct redir_cmd *redir = NULL;
-
-    char *tok;
-    tok = strtok(cmd_str," \n");
-    int tok_index = 0;
-    while(tok != NULL) {
-        if ((strcmp(tok, "<") == 0) || (strcmp(tok, ">") == 0)) {
-            if (redir == NULL) {
-                redir = malloc(sizeof(struct redir_cmd));
-                redir->redir_type = (strcmp(tok, "<") == 0) ? REDIR_IN : REDIR_OUT;
+// cat < output.txt | sort | uniq | wc
+int main() {
+    char cmd_string[48];
+    int command_size = 10;
+    struct cmd_exe_struct *cmd_exe_ptr = malloc(sizeof(struct cmd_exe_struct));
+    while (1) {
+        int is_redirected = 0;
+        printf("$");
+        fgets(cmd_string, sizeof(cmd_string), stdin);
+        char *cmd_arr[command_size];
+        cmd_string[strcspn(cmd_string,"\n")] = '\0';
+        char *token = strtok(cmd_string, " ");
+        int tokens_count = 0;
+        if (strcmp(token, "cd") == 0) {
+            printf("Feature is yet to be implemented\n");
+        } else {
+            while(token != NULL) {
+                if (strcmp(token,">") == 0 || strcmp(token, "<") == 0) {
+                    is_redirected = 1;
+                    break;
+                }
+                cmd_exe_ptr->args[tokens_count] = token;
+                tokens_count ++;
+                token = strtok(NULL," ");
             }
-            tok = strtok(NULL, " \n"); 
-            redir->file_path = tok;
-        }
-
-        else {
-            argv[tok_index] = tok;
-            tok_index++;
-
-        }
-        tok = strtok(NULL, " \n");
-
-    }
-    argv[tok_index] = NULL;
-    cmd_struct->cmd_type = (redir != NULL) ? REDIRECT : TRIVIAL;
-    cmd_struct->argv = argv;
-    cmd_struct->redir = redir;
-    return cmd_struct;
-}
-
-int main(){
-    char commandBuf[MAX_BUFF_SIZE];
-    char *argv[MAX_ARGS];
-    struct cmd *cmd;
-    while(1) {
-        printf("Unix.Shell 0.1>>");
-        fflush(stdout);
-        fgets(commandBuf,MAX_BUFF_SIZE,stdin);
-        if (strcmp(commandBuf,"\n") != 0) {
-            cmd = parse_cmd(commandBuf, argv); 
-            exe_cmd(cmd);
-            free(cmd->redir);
-            free(cmd);
+            cmd_exe_ptr->args[tokens_count] = NULL;
+            cmd_exe_ptr->is_redirected = 0;
+            if(is_redirected == 1) {
+                int redir_type = strcmp(token,">") == 0 ? 1 : 0;
+                char* file_path =  strtok(NULL," ");
+                if (file_path == NULL) {
+                    perror("Please input valid file path to redirect");
+                    exit(1);
+                };
+                cmd_exe_ptr->redir_type = redir_type;
+                cmd_exe_ptr->file_path = file_path;
+                cmd_exe_ptr->is_redirected = 1;
+            }
+            execute_cmd(cmd_exe_ptr);
+            free(cmd_exe_ptr); //free the allocated memory for command arguments
         }
     }
+    
 }
